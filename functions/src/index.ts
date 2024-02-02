0073dc7214954173aeb91ce01339d263ea318a09
Joel del Castillo <joel.del.castillo09@gmail.com>
2024-02-02 09:43:41 -0500

    Partially fixed token creation insecurity

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,8 @@ import { initializeApp } from "firebase-admin/app";
 import { onCustomEventPublished } from "firebase-functions/v2/eventarc";
 import { Timestamp, getFirestore } from "firebase-admin/firestore";
 import { onDocumentCreated, onDocumentUpdated } from "firebase-functions/v2/firestore";
+// eslint-disable-next-line camelcase, import/default
+import { jwtDecode } from "jwt-decode";
 import Expo from "expo-server-sdk";
 
 
@@ -13,17 +15,23 @@ initializeApp();
 const db = getFirestore();
 const expo = new Expo();
 
-
 export const createCustomAzureToken = onCall(async (request) => {
+    const decodedToken = jwtDecode(request.data.token) as Token;
+
     const additionalClaims = {
-        email: request.data.email,
-        name: request.data.name,
+        tokenType: "azure",
+        token: request.data.token,
+        email: decodedToken.upn,
+        name: decodedToken.name,
     };
     logger.info("New Token Request: ", request.data);
+
+    // Check here if token is legitimate
     try {
+        if (!decodedToken.oid) return;
         const token = await getAuth()
-            .createCustomToken(request.data.uid, additionalClaims);
-        logger.info("Token Created for", request.data.email, token);
+            .createCustomToken(decodedToken.oid, additionalClaims);
+        logger.info("Token Created for", decodedToken.upn);
         return {
             customToken: token,
             status: "success",
@@ -114,53 +122,87 @@ export const notificationSender = onDocumentCreated("users/{usersId}/notificatio
     return null;
 });
 
+export interface Token {
+    // issuer
+    iss: string | undefined;
+    // issued at (seconds since Unix epoch)
+    iat: number | undefined;
+    // not valid before (seconds since Unix epoch)
+    nbf: number | undefined;
+    // expiration time (seconds since Unix epoch)
+    exp: number | undefined;
+    // Collider
+    app_displayname: string | undefined;
+    // Collider id
+    appid: string | undefined;
+    // Del Castillo Baquero
+    family_name: string | undefined;
+    // Joel
+    given_name: string | undefined;
+    // IP Address
+    ipaddr: string | undefined;
+    // Joel Del Castillo Baquero
+    name: string | undefined;
+    // 7777e6ef-be73-4f48-b860-3508a0e647c6
+    oid: string | undefined;
 
-// import * as functions from "firebase-functions";
-// import * as admin from "firebase-admin";
+    tenant_region_scope: string | undefined;
 
-// admin.initializeApp();
+    // jdelcastillo@estud.usfq.edu.ec
+    unique_name: string | undefined;
+    // jdelcastillo@estud.usfq.edu.ec
+    upn: string | undefined;
+}
 
-// const firestore = admin.firestore();
 
-// export const createNotificationsOnGroupUpdate = functions.firestore
-//     .document("groups/{groupId}")
-//     .onUpdate(async (change, context) => {
-//         const beforeData = change.before.data() as GroupType;
-//         const afterData = change.after.data() as GroupType;
+// export const createNotificationsOnGroupUpdate = onDocumentUpdated("groups/{groupId}", async (event) => {
+//     const beforeData = event.data?.before?.data() ?? {};
+//     const afterData = event.data?.after?.data() ?? {};
 
-//         // Check if the invited list has been updated
-//         if (beforeData.invited !== afterData.invited) {
-//             const newInvited = getNewInvitedMembers(beforeData.invited, afterData.invited);
+//     // Check if the invited list has been updated
+//     if (beforeData.invited !== afterData.invited) {
+//         const newInvited = getNewInvitedMembers(beforeData.invited, afterData.invited);
 
-//             // Create notifications for new invited members
-//             await createNotifications(context.params.groupId, newInvited);
-//         }
-//     });
+//         // Create notifications for the group organizers
+//         await createNotification(event.params.groupId, newInvited, "newMember");
+//     }
+// });
+
+// export const createNotificationsOnGroupNewEvent = onDocumentUpdated("groups/{groupId}", async (event) => {
+//     const beforeData = event.data?.before?.data() ?? {};
+//     const afterData = event.data?.after?.data() ?? {};
+
+//     // Check if the invited list has been updated
+//     if (beforeData.events !== afterData.events) {
+//         const newEventId = getNewEvent(beforeData.events as string[], afterData.events as string[]);
+//         afterData.invited.forEach(async (userId: string) => {
+//             // Create notifications for the group organizers
+//             await createEventNotification(userId, event.params.groupId, newEventId);
+//         });
+//     }
+// });
 
 // // eslint-disable-next-line require-jsdoc
-// async function createNotifications(groupId: string, invitedMembers: string[]) {
-//     const notificationsCollection = firestore.collection("/users/notifications");
+// function getNewEvent(beforeInvited: string[], afterInvited: string[]): string[] {
+//     return afterInvited.filter((userId) => !beforeInvited.includes(userId));
+// }
 
-//     for (const userId of invitedMembers) {
-//         // Check if a notification document already exists for the user
-//         const existingNotification = await notificationsCollection
-//             .doc(userId)
-//             .collection("groupInvitations")
-//             .doc(groupId)
-//             .get();
 
-//         if (!existingNotification.exists) {
-//             // Create a new notification document for the user
-//             await notificationsCollection
-//                 .doc(userId)
-//                 .collection("groupInvitations")
-//                 .doc(groupId)
-//                 .set({
-//                     groupId,
-//                     timestamp: admin.firestore.FieldValue.serverTimestamp(),
-//                 });
-//         }
-//     }
+// // eslint-disable-next-line require-jsdoc
+// async function createEventNotification(userId: string, groupId: string, eventId: string) {
+//     const notificationsCollectionRef = db.collection("users").doc(userId).collection("notifications");
+//     notificationsCollectionRef.add({
+//         createdAt: Timestamp.now(),
+//         updatedAt: Timestamp.now(),
+//         updatedBy: "server",
+//         uid: "",
+//         type: "newEvent5",
+//         status: "unread",
+//         event: eventId,
+//         group: groupId,
+//         message: "Tu Grupo ha creado un nuevo Evento",
+//         opened: false,
+//     });
 // }
 
 // // eslint-disable-next-line require-jsdoc
